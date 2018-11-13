7b029a641637328f12a17e6203de7e844daf1b5b
Alexandre <alexandre.bailly@live.fr>
2018-11-13 16:31:33 +0100

    test on services

--- DependencyInjection/KodmitUserExtension.php
+++ DependencyInjection/KodmitUserExtension.php
@@ -4,14 +4,18 @@ namespace Kodmit\UserBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Yaml\Yaml;
 
 class KodmitUserExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
 
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
+        // Update services
+        $service_path = "../../config/services.yaml";
+        $services = Yaml::parseFile($service_path);
+        array_push($services, ["test" => ["value" => "ok"]]);
+        $yaml = Yaml::dump($services);
+        file_put_contents($service_path, $yaml);
     }
 }
