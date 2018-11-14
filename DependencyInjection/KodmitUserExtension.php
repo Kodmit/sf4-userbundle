b1371dd5aa8bb4219384e6a013cc889395652a34
Alexandre <alexandre.bailly@live.fr>
2018-11-14 10:13:07 +0100

    service

--- DependencyInjection/KodmitUserExtension.php
+++ DependencyInjection/KodmitUserExtension.php
@@ -2,19 +2,29 @@
 
 namespace Kodmit\UserBundle\DependencyInjection;
 
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Bundle\MakerBundle\Util\YamlSourceManipulator;
 use Symfony\Bundle\MakerBundle\Generator;
 use Symfony\Bundle\MakerBundle\ConsoleStyle;
 
-class KodmitUserExtension extends Extension
+class KodmitUserExtension extends Extension implements ExtensionInterface
 {
 
-    private $consoleStyle;
+    private $generator;
 
-    public function load(array $configs, Generator $generator)
+    public function getAlias()
     {
+        return "user_extension";
+    }
+
+
+    public function load(array $configs, ContainerBuilder $container)
+    {
+
+
+        $generator = $container->get("maker.generator");
 
         $yamlSource = 'config/services.yaml';
 
