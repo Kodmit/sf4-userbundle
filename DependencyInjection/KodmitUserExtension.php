07f7597512d1993b7aa6ed18df4c6d88689d7f2e
Alexandre <alexandre.bailly@live.fr>
2018-11-14 18:21:27 +0100

    autoupdate services

--- DependencyInjection/KodmitUserExtension.php
+++ DependencyInjection/KodmitUserExtension.php
@@ -2,6 +2,10 @@
 
 namespace Kodmit\UserBundle\DependencyInjection;
 
+use App\Kernel;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -15,6 +19,16 @@ class KodmitUserExtension extends Extension implements ExtensionInterface
 
     public function load(array $configs, ContainerBuilder $container)
     {
+        $kernel = new Kernel();
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput(array(
+            'command' => 'kodmit:userbundle:init'
+        ));
+
+        $output = new BufferedOutput();
+        $application->run($input, $output);
 
     }
 
